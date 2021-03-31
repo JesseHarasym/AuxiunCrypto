b538eed047685a30a38d254fb7f54ead8e7e9829
JesseHarasym <51863533+JesseHarasym@users.noreply.github.com>
2021-03-31 07:17:43 -0600

    Delete AuxiunCrypto directory

--- AuxiunCrypto/migrations/2_deploy_contract.js
+++
@@ -1,9 +0,0 @@
-const GameAssets = artifacts.require("GameAssets");
-const Coin = artifacts.require("Coin");
-const MultiToken = artifacts.require("MultiToken");
-
-module.exports = function(deployer) {
-  deployer.deploy(GameAssets);
-  deployer.deploy(Coin);
-  deployer.deploy(MultiToken);
-};
