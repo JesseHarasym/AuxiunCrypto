4040fe9eac95871948d4f694bad8ffc4f3b0ac39
mgreen821 <55030708+mgreen821@users.noreply.github.com>
2021-04-11 13:21:21 -0600

    Updated project

--- backend/routes/marketplace.js
+++ backend/routes/marketplace.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 // const router = require("./users");
 const erc721 = require("../web3/ERC721_Web3");
+const erc1155 = require("../web3/ERC1155_Web3");
 const AssetsToken = require("../models/assetsTokenSchema.js");
 //const Marketplace = require("../models/marketplaceSchema.js");
 const jwt = require("jsonwebtoken");
@@ -40,7 +41,7 @@ router.route("/asset/list/:assetId").post(verify, async (req, res) => {
       else {
         res.json({
           success: false,
-          msg: "User does not own asset:" + req.params.assetId
+          msg: "User does not own asset:" + req.params.assetId,
         });
       }
     })
@@ -63,7 +64,7 @@ router.route("/asset/list/:assetId").post(verify, async (req, res) => {
     .then(() =>
       res.json({
         success: true,
-        msg: "Successfully listed" + req.params.assetId
+        msg: "Successfully listed" + req.params.assetId,
       })
     )
     //On Fail respond with failure json as per Brads API Schema
@@ -74,18 +75,29 @@ router.route("/asset/list/:assetId").post(verify, async (req, res) => {
 
 router.route("/assets").get((req, res) => {
   // Return all assets listed in the marketplace
+  let uri;
   AssetsToken.find({ inmarketplace: true })
     .then(async (assets) => {
       //TODO grab ipfs URI from the blockchain token id
       const cidList = [];
       if (assets.length > 0) {
         for (let i = 0; i < assets.length; i++) {
-          const uri = await erc721
-            .getTokenURI(assets[i].token)
-            .then((result) => {
-              return result;
-            })
-            .catch((err) => res.status(400).json("Error: " + err));
+          if (assets.batchtoken == "true") {
+              uri = await erc1155
+              .getTokenURI(assets[i].token)
+              .then((result) => {
+                return result;
+              })
+              .catch((err) => res.status(400).json("Error: " + err));
+          } else {
+              uri = await erc721
+              .getTokenURI(assets[i].token)
+              .then((result) => {
+                return result;
+              })
+              .catch((err) => res.status(400).json("Error: " + err));
+          }
+
           const tokenObject = await catJson(uri);
           tokenObject.tokenId = assets[i].token;
           cidList.push(tokenObject);
@@ -104,13 +116,13 @@ router.route("/add").post((req, res) => {
     token: req.body.token,
     owner: req.body.owner,
     inmarketplace: false,
-    price: req.body.price
+    price: req.body.price,
   });
 
   asset.save().then(() =>
     res.json({
       success: true,
-      msg: "Successfully added " + asset.token
+      msg: "Successfully added " + asset.token,
     })
   );
 });
